b7281f3208f168bc281b1f8af828fd4e17c62708
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-07-06 13:07:03 -0400

    more js work

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,53 @@
 //Current time and date
 var currentDate = moment().format('MMMM Do YYYY');
 $('#currentDay').html(currentDate);
+console.log(currentDate)
+
+//Current hour
+var currentHour = moment().format('hA');
+console.log(currentHour)
+
+//Kind of works
+//Need loop to check all boxes?
+$(".hour").filter(function() {
+    return $(this).text().trim() < currentHour;
+  })
+  .nextAll(".description")
+  .first()
+  .addClass("past");
+
+$(".hour").filter(function() {
+    return $(this).text().trim() === currentHour;
+  })
+  .nextAll(".description")
+  .first()
+  .addClass("present");
+
+  $(".hour").filter(function() {
+    return $(this).text().trim() > currentHour;
+  })
+  .nextAll(".description")
+  .first()
+  .addClass("future");
+
+//Need to check box hours against current hour
 
 
+/* Doesn't work
+if (document.getElementById('col-md-1 hour') < currentHour) {
+    $('col-md-1 hour').addclass('past');
+}
 
+else if (document.getElementById('col-md-1 hour') === currentHour) {
+    $('col-md-1 hour').addclass('present');
+}
 
-//Need to check box hours against current time
+else if (document.getElementById('col-md-1 hour') > currentHour) {
+    $('col-md-1 hour').addclass('future');
+}
+*/
 
 //Colors and names already provided in style sheet
-//if (currentHour is NOW) then (box color = RED) else if (currentHour is PAST) then (box color = GREY) else if (currentHour is FUTURE) then (box color = GREEN)
+//if (currentHour is NOW) then (box color = RED (css .present)) else if (currentHour is PAST) then (box color = GREY (css. past)) else if (currentHour is FUTURE) then (box color = GREEN (css .future))
+
+//Save Button listeners and local storage
